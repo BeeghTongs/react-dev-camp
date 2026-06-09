0d0ce695cf43877313569d95e76ace7c390f3d0b
Ntokozo Tonga <ntokozo.tonga@entelect.co.za>
2026-06-09 14:29:32 +0200

    started product card component creation

--- Financial Product App/src/pages/ProductPage.jsx
+++ Financial Product App/src/pages/ProductPage.jsx
@@ -1,7 +1,42 @@
+import './ProductPage.css';
+import ProductCard from '../components/ProductCard';
+import headphone from '../assets/headphone.png';
+
 function ProductPage() {
+const products = [
+  {
+    id: 1,
+    imageUrl: headphone,
+    title: "Wireless Headphones",
+    price: "R350 p/m",
+  },
+  {
+    id: 2,
+    imageUrl: headphone,
+    title: "Gaming Mouse",
+    price: "R120 p/m",
+  },
+  {
+    id: 3,
+    imageUrl: headphone,
+    title: "Mechanical Keyboard",
+    price: "R250 p/m",
+  },
+];
+
   return (
     <div>
-      <h1>Product Page</h1>
+      <header> Product Page </header>
+      <div className="recommended-products">
+            {products.map((product) => (
+            <ProductCard
+            key={product.id}
+            imageUrl={product.imageUrl}
+            title={product.title}
+            price={product.price}
+            />
+        ))}
+      </div>
     </div>
   );
 }
